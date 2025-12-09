c304d0090221cfa5c9109f871450879ff41652ec
Arju-Hasan <rajuahmeed698@gmail.com>
2025-12-10 00:40:50 +0600

    add tutors delete api

--- index.js
+++ index.js
@@ -222,6 +222,14 @@ app.get('/tutors/:id',  async (req, res) => {
   }
 });
 
+  app.delete('/tutors/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const result = await tutorsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
 
